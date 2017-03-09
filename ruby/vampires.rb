2c344c3368c14cd44a8d63af9eef3a0b9865fb2b
papistan <jpapisan@gmail.com>
2017-03-08 21:25:56 -0800

    add detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,59 @@
+
+
 p "What is your name?"
-name = gets.chomp
+name = gets.chomp.capitalize
 
 p "How old are you?"
 age = gets.chomp.to_i
 
-p "What year born?"
+p "What year where you born?"
 birthdate = gets.chomp.to_i
 
 p "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+garlic = gets.chomp.capitalize 
 
-p "Health insurance?"
-insurance = gets.chomp
+p "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp.capitalize
+
+
+vampire = “Results inconclusive.”
+
+# age correct
+if age = (2017 - birthdate) || (2016 - birthdate)
+	age_correct = true
+else age_correct = false
+end 
+
+# garlic 
+if garlic == "YES" 
+	eat_garlic = true
+else eat_garlic = false
+end
+
+# insurance 
+if insurance == "YES"
+	want_insurance = true
+else want_insurance = false
+end
+
+
+if age_correct && (eat_garlic || want_insurance)
+	vampire = “Probably not a vampire.”
+end
+
+if (age_correct = false) && (eat_garlic = false || want_insurance = false)
+	vampire = “Probably a vampire.”
+end 
+
+if (age_correct = false) && (eat_garlic = false) && (want_insurance = false)
+	vampire = “Almost certainly a vampire.”
+end
+
+if name == (“Drake Cula” || “Tu Fang”)
+	vampire = “Definitely a vampire.”
+end
+
+print vampire
+
+
 
