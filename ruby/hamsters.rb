a6e13395f09139121a7fbdc6f7391a63e4de5275
papistan <jpapisan@gmail.com>
2017-03-09 09:27:38 -0800

    Add intake questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "On a scale of 1 to 10, with 10 loudest, is there a volume level that you prefer?"
+prefered_volume = gets.chomp.to_i
+
+puts "What is your fur color?"
+fur_color = gets.chomp
+
+puts "Are you a good candidate for adoption? yes/no"
+adoptable = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
