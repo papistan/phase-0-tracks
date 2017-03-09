f6152e41b9f8b55cc08c2005062aa3dc2f0f61a2
papistan <jpapisan@gmail.com>
2017-03-08 18:59:26 -0800

    add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+p "What is your name?"
+name = gets.chomp
+
+p "How old are you?"
+age = gets.chomp.to_i
+
+p "What year born?"
+birthdate = gets.chomp.to_i
+
+p "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+p "Health insurance?"
+insurance = gets.chomp
+
