47598581e15d3883348222d62b2d5f402b82db29
papistan <jpapisan@gmail.com>
2017-03-17 16:11:53 -0700

    Add user interface and data structure

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,8 +6,6 @@
 
 def changer(real_name)
  downcase_name = real_name.downcase
-
-# def run(downcase_name)
 	counter = 0
 	new_name = ""
 	vowels_str = "aeiou"
@@ -22,7 +20,7 @@ def changer(real_name)
 		   new_name[counter] = "a"
 		elsif vowels_str.include?(downcase_name[counter])
 		   letter = downcase_name[counter]
-		   puts vowels_str.index(letter) + 1
+		   # puts vowels_str.index(letter) + 1
 		   new_name[counter] = vowels_str[vowels_str.index(letter) + 1]
 	 	elsif downcase_name[counter] == "z"
 	 		new_name[counter] = "b"
@@ -32,22 +30,44 @@ def changer(real_name)
 	 	end 
 		counter += 1
 	end
-	puts new_name
+	# puts new_name
 # enp
 
  new_name = new_name.split(' ').reverse!
  new_name = "#{new_name[0]} #{new_name[1]}"
- # new_last = new_name[0].split('')
- # new_first = new_name[1].split('')
-p new_name
- # first_counter = 0 
- # def flipper(x)
- # 	counter = 0 
- # 	while counter < x.length
 
+# p new_name
 
 end
 
-changer("Felicia Torres")
+real_name = "empty"
+
+counter = 0
+repeat_counter = "open"
+real_namez = []
+alias_namez = []
+sub_counter = 0
+
+Until repeat_counter = "closed"
+	puts "Type in your real name to get your alias name. When done type 'quit'"
+	real_name = gets.chomp
+
+# THIS CHUNK IS CAUSING ME PROBLEMS BUT RAN OUT OF TIME
+	if real_name == "quit"
+	 alias_namez.each {|i| 
+	 	print real_namez[sub_counter] " is also " alias_namez[sub_counter]
+	 	sub_counter +=1
+	 }
+	 repeat_counter = "closed"
+# THIS CHUNK IS CAUSING ME PROBLEMS BUT RAN OUT OF TIME
+
+	else 
+	puts "Type in your real name to get your alias name. When done type 'quit'"
+	real_name = gets.chomp
+	real_namez[counter] = real_name
+	puts "Your new alias is #{changer(real_name)}"
+	alias_namez[counter] = changer(real_name)
+	end
+end
 
 
