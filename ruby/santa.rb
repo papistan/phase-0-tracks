5919b15d030f3f02b85c7118f67e12eea022bdde
papistan <jpapisan@gmail.com>
2017-03-21 14:48:39 -0700

    Add new class Santa w/ 3 attributes

---
+++ ruby/santa.rb
@@ -0,0 +1,23 @@
+
+
+class Santa
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!" 
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+
+end
+
+redhat = Santa.new
+
+redhat.speak
+
+redhat.eat_milk_and_cookies("choc chip")
