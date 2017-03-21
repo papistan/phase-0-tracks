46a7da4b8119681f92c65a9f570d16d0b9544150
papistan <jpapisan@gmail.com>
2017-03-21 16:23:28 -0700

    Add initializations for gender and ethnicity and other attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -10,14 +10,31 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+	def about
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Gender: #{@gender}"
 	end
 
 end
 
-redhat = Santa.new
+redhat = []
 
-redhat.speak
+example_genders = ["male", "female", "trans", "non", "furry"]
 
-redhat.eat_milk_and_cookies("choc chip")
+example_ethnicity = ["mestizo", "white", "black", "what", "human"]
+
+redhat << Santa.new(example_genders[4], example_ethnicity[1])
+
+redhat << Santa.new(example_genders[3], example_ethnicity[2])
+
+redhat[0].about
+
+redhat[1].about
