19653a31219a7f396b26c0cb3c80a020537a9a02
papistan <jpapisan@gmail.com>
2017-03-31 14:04:48 -0700

    add function to create random number of letters in strings of command

--- js/algos.js
+++ js/algos.js
@@ -25,6 +25,30 @@ function share(x,y) {
 	return value
 }
 
-var blerg = {name: "Steven", age: 54};
-var tamir = {name: "Tamir", age: 54};
-share(blerg, tamir);
+
+// Define a funtion 'arrayCreator' that takes an integer
+function arrayCreator(y) {
+x = y
+// make a new empty array
+var newArray = [];
+// For integer number times
+	for (var i = 0; i < x; i++) { 
+	// a new string of random letters,
+	// make number of random letters also be random between 1..10. 
+	    var letters = "abcdefghijklmnopqrstuvwxyz";
+		var newString = "";
+	    randomNumberToTen = Math.floor(Math.random() * 10);
+	    for (var j = 0; j < randomNumberToTen; j++) {
+	    newString += letters.charAt(Math.floor(Math.random()*letters.length));
+	    }
+	// Put into the new empty array
+	    newArray.push(newString);
+	}
+	return newArray
+}
+
+console.log(arrayCreator(3));
+
+// var blerg = {name: "Steven", age: 54};
+// var tamir = {name: "Tamir", age: 54};
+// share(blerg, tamir);
