532fbbc426ea89900d146ae820ff332660820198
papistan <jpapisan@gmail.com>
2017-04-10 21:55:43 -0700

    created expelled function to randomly expell student or delete specific students

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,28 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/expelled' do
+  erb :expelled
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/students/expelled' do
+	puts "Expelling #{params['name']}" 
+	 db.execute("DELETE FROM students WHERE name=?", params['name'])
+  redirect '/'
+end
+
+post '/students/expelRandom' do
+	poor_student = db.execute("SELECT name FROM students").sample
+	p "You've expelled #{poor_student['name']}" 
+ #  redirect '/'
+end
+
 
 # add static resources
